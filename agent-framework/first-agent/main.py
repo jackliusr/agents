52e2fed4ec7c8bf606e38c67e28e2ab8584100ad
Jack Liu Shurui <jack.liu.s.r@gmail.com>
2026-04-23 21:23:07 +0800

    feat: agent using microsoft agent-framework and ollama, fail due to framework issue, later do it

--- agent-framework/first-agent/main.py
+++ agent-framework/first-agent/main.py
@@ -1,6 +1,18 @@
-def main():
-    print("Hello from first-agent!")
+import asyncio
+from agent_framework.openai import OpenAIChatClient
+from dotenv import load_dotenv
 
+load_dotenv()  # Loads variables from .env
 
-if __name__ == "__main__":
-    main()
+import asyncio
+from agent_framework.ollama import OllamaChatClient
+
+async def main():
+    agent = OllamaChatClient().as_agent(
+        name="HelpfulAssistant",
+        instructions="You are a helpful assistant running locally via Ollama.",
+    )
+    result = await agent.run("What is the largest city in France?")
+    print(result)
+
+asyncio.run(main())  
