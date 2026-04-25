d21ef7fc59faa8224cb857be6ead3c67cba9932e
Jack Liu Shurui <jack.liu.s.r@gmail.com>
2026-04-25 10:33:18 +0800

    feat: use ollama and qwen3.6

--- adk/multi_tool_agent/agent.py
+++ adk/multi_tool_agent/agent.py
@@ -1,20 +1,9 @@
-# Copyright 2026 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+
 
 import datetime
 from zoneinfo import ZoneInfo
 from google.adk.agents import Agent
+from google.adk.models.lite_llm import LiteLlm
 
 def get_weather(city: str) -> dict:
     """Retrieves the current weather report for a specified city.
@@ -70,7 +59,7 @@ def get_current_time(city: str) -> dict:
 
 root_agent = Agent(
     name="weather_time_agent",
-    model="gemini-2.5-flash",
+    model=LiteLlm(model="ollama_chat/qwen3.6:35b-a3b-q4_K_M"),
     description=(
         "Agent to answer questions about the time and weather in a city."
     ),
--- adk/my_agent/agent.py
+++ adk/my_agent/agent.py
@@ -1,4 +1,5 @@
 from google.adk.agents.llm_agent import Agent
+from google.adk.models.lite_llm import LiteLlm
 
 # Mock tool implementation
 def get_current_time(city: str) -> dict:
@@ -7,7 +8,8 @@ def get_current_time(city: str) -> dict:
 
 
 root_agent = Agent(
-    model='gemini-2.5-flash',
+    #model='gemini-2.5-flash',
+    model=LiteLlm(model="ollama_chat/qwen3.6:35b-a3b-q4_K_M"),
     name='root_agent',
     description="Tells the current time in a specified city.",
     instruction="You are a helpful assistant that tells the current time in cities. Use the 'get_current_time' tool for this purpose.",
